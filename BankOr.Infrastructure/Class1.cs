ed9e25fb80cc6e9f393bd4fbf29b647cb36c8537
Mats Andersson <mats.andersson@santanderconsumer.se>
2018-04-24 15:51:44 +0200

    Upgraded to 2.0.0 RTM

---
+++ BankOr.Infrastructure/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Orleans;
+using Orleans.Providers;
+using Orleans.Runtime;
+using Orleans.Storage;
+
+namespace BankOr.Infrastructure
+{
+    public class BankOrStorageProvider : IStorageProvider
+    {
+        public Task Init(string name, IProviderRuntime providerRuntime, IProviderConfiguration config)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name { get; }
+        public Task ReadStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task WriteStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ClearStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Logger Log { get; }
+    }
+}
