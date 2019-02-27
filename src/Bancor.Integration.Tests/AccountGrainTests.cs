5de884d514bcd202ca7de2d178f88e8549403ebe
Mats <mats@greyhouse.se>
2019-02-27 18:26:41 +0100

    Moved tests to separate file

--- src/Bancor.Integration.Tests/AccountGrainTests.cs
+++ src/Bancor.Integration.Tests/AccountGrainTests.cs
@@ -1,52 +1,11 @@
 ﻿using Bancor.Core;
-using Bancor.Core.Grains.Interfaces;
 using Bancor.Core.Grains.Interfaces.Grains;
 using Orleans.Transactions;
 using Shouldly;
-using System;
 using Xunit;
 
 namespace Bancor.Integration.Tests
 {
-
-    public class JournaledGrainTest : GrainTest
-    {
-        [Fact]
-        public async void Should_Store_One_Event()
-        {
-            // Arrange
-            await DeployClusterAsync();
-            var accountId = Guid.Parse("EBD09F9C-4A99-4B8D-A581-3C93764D24B1");
-            var grain = TestCluster.GrainFactory.GetGrain<IJournaledAccountGrain>(accountId);
-
-            // Act
-            await grain.Deposit(200);
-            await grain.Deposit(200);
-
-            // Assert
-            (await grain.Balance()).ShouldBe(500);
-        }
-
-
-        [Fact]
-        public async void Should_Calculate_Correct_Balance_When_Four_Events_Applied()
-        {
-            // Arrange
-            await DeployClusterAsync();
-            var accountId = Guid.Parse("B274FB90-1213-4333-85A1-1A69FF268022");
-            var grain = TestCluster.GrainFactory.GetGrain<IJournaledAccountGrain>(accountId);
-
-            // Act
-            await grain.HasName("Savings account");
-            await grain.Deposit(200);
-            await grain.Deposit(200);
-            await grain.Withdraw(400);
-                
-            // Assert
-            (await grain.Balance()).ShouldBe(0);
-        }
-    }
-
     public class AccountGrainTests : GrainTest
     {
         [Fact]
---
+++ src/Bancor.Integration.Tests/JournaledGrainTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Bancor.Core.Grains.Interfaces;
+using Shouldly;
+using Xunit;
+
+namespace Bancor.Integration.Tests
+{
+    public class JournaledGrainTest : GrainTest
+    {
+        [Fact]
+        public async void Should_Store_One_Event()
+        {
+            // Arrange
+            await DeployClusterAsync();
+            var accountId = Guid.Parse("EBD09F9C-4A99-4B8D-A581-3C93764D24B1");
+            var grain = TestCluster.GrainFactory.GetGrain<IJournaledAccountGrain>(accountId);
+
+            // Act
+            await grain.Deposit(200);
+            await grain.Deposit(200);
+
+            // Assert
+            (await grain.Balance()).ShouldBe(500);
+        }
+
+
+        [Fact]
+        public async void Should_Calculate_Correct_Balance_When_Four_Events_Applied()
+        {
+            // Arrange
+            await DeployClusterAsync();
+            var accountId = Guid.Parse("B274FB90-1213-4333-85A1-1A69FF268022");
+            var grain = TestCluster.GrainFactory.GetGrain<IJournaledAccountGrain>(accountId);
+
+            // Act
+            await grain.HasName("Savings account");
+            await grain.Deposit(200);
+            await grain.Deposit(200);
+            await grain.Withdraw(400);
+                
+            // Assert
+            (await grain.Balance()).ShouldBe(0);
+        }
+    }
+}
