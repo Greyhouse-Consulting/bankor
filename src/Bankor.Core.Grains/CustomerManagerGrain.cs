b8c6fe09e7103c573a9130d4a4b2c4e02b0e8e7a
Mats Andersson <mats.andersson@santanderconsumer.se>
2019-07-02 11:51:36 +0200

    Remoing usage of customerrepository

--- src/Bankor.Core.Grains/CustomerManagerGrain.cs
+++ src/Bankor.Core.Grains/CustomerManagerGrain.cs
@@ -1,19 +1,12 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Bancor.Core.Grains.Interfaces.Grains;
-using Bancor.Core.Grains.Interfaces.Repository;
 using Orleans;
 
 namespace Bancor.Core.Grains
 {
     public class CustomerManagerGrain : Grain, ICustomerManagerGrain
     {
-        private readonly ICustomerRepository _customerRepository;
-
-        public CustomerManagerGrain(ICustomerRepository customerRepository)
-        {
-            _customerRepository = customerRepository;
-        }
         public async Task<ICustomerGrain> Create(string name)
         {
             var customer = new Customer
